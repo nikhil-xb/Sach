d8e44cd2a3c9890e19031f352e8588a15b41147f
Nikhil Kumar <77227336+nikhil-xb@users.noreply.github.com>
2023-02-06 23:06:54 +0530

    update resume tag

--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -78,7 +78,7 @@ const About = () => {
               <Text> After spending around 8 hours a day studying, coding and completing projects, I realized that I'd learnt a lot more in just a couple of months than I did in my entire degree program.</Text>
               <Text> Learning the fundamentals of statistics, probability, programming and business analytics is no easy task. These are entire fields on their own, and it would take a lifetime to master even one of them.</Text>
               <Text>  This blog will serve as a home for all my thoughts, notes and experiences ranging from Programming, Math, Machine Learning and Computer Vision.  </Text>
-              <ResumeButton href="Nikhil+Kumar's+Cv.pdf" target="_blank"> Download resume </ResumeButton>
+              <ResumeButton href="Nikhil Kumar's Cv.pdf" target="_blank"> Download resume </ResumeButton>
             </div>
             
           </AboutSection>
